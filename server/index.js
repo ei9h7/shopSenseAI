1f90c96e20ef282b9de1d0b60a1130a79cee6655
ei9h7 <aytonmaceachern@me.com>
2025-06-30 01:09:20 -0600

    Fix CORS configuration for HTTPS localhost

--- server/index.js
+++ server/index.js
@@ -21,7 +21,10 @@ app.use(cors({
             /^https:\/\/.*\.stackblitz\.app$/,
             /^https:\/\/.*\.bolt\.new$/
           ]
-        : ['http://localhost:5173'],
+        : [
+            'http://localhost:5173',
+            'https://localhost:5173'
+          ],
     credentials: true
 }));
 app.use(express.json({ limit: '1mb' }));
